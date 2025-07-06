b9e4901e8849c2793e963bc15b266338530cc15c
kipronom130 <yodochris22@gmail.com>
2025-07-06 06:57:19 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 //===============================LAKIYA-MD===================================
-SESSION_ID: process.env.SESSION_ID === undefined ? 'Lakiya-Md=bIkUzRYQ#QMAKLcj6mMcqK-kgX2qFnIXuyax5vNdeT_WPlP3dB38' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'Lakiya-Md=SV8WgCRZ#YXA6e4_pV1mkBYxU1Ly49J3SvJ2bw1NfqDo8YF3TneU' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ?"true" : process.env.AUTO_READ_STATUS,
 MODE: process.env.MODE === undefined ?"privet" : process.env.MODE,
